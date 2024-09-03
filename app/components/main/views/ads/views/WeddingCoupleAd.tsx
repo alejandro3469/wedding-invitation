2c3235ffa8a4507cb5f3cc9f3ce3bc18a00f12b6
alejandro3469 <alejandro.perez3469@gmail.com>
2024-09-02 23:23:52 -0600

    Add wedding-couple ad

--- app/components/main/views/ads/views/WeddingCoupleAd.tsx
+++ app/components/main/views/ads/views/WeddingCoupleAd.tsx
@@ -8,9 +8,26 @@ const AddWeddingCouple = () => {
     <AdLayout>
       <div className={styles.container}>
         <span className={styles.text_container}>
-          <span className={styles.text_main_line}>Invitacion en un link para tu boda</span>
+          <span className={styles.text_main_line}>Invitacion para tu boda en un link</span>
           <span>Compartela en clicks!</span>
           <span className={styles.text_moving_adjectives}>Aesthetic, Fast</span>
+          <span className={styles.dented_circle_container}>
+            <svg className={styles.dented_circle} viewBox="0 0 180 180">
+              <defs>
+                <path id="dentedPath" d="
+                M 90, 90
+                m -45, 0
+                a 45,45 0 1,1 90,0
+                a 45,45 0 1,1 -90,0
+            " />
+              </defs>
+              <text>
+                <textPath href="#dentedPath">
+                  Entrega inmediata
+                </textPath>
+              </text>
+            </svg>
+          </span>
         </span>
         <Image
           className={''}
