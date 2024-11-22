c47f47cc142cab3a498c3370e3894d73f4a71c49
jorge.ab <alejandro.perez3469@gmail.com>
2024-11-21 19:04:26 -0600

    add save image sizes

--- app/components/wedding-invitations/first-invitation/components/InvitationBanner.tsx
+++ app/components/wedding-invitations/first-invitation/components/InvitationBanner.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from '../styles/InvitationBanner.module.css';
-import {Karla, Montserrat, Montserrat_Alternates } from "next/font/google";
-
 import localFont from 'next/font/local';
 
 
 
-const montserrat = Montserrat({
-    weight: ['300'],
-    style: ['normal', 'italic'],
-    subsets: ['latin'],
-    display: 'swap',
-});
-
-const karla = Karla({
-    subsets: ["latin"],
-});
-
-const mon = localFont({ src: './Montserrat-VariableFont_wght.ttf' })
+const Montserrat = localFont({ src: './Montserrat-VariableFont_wght.ttf' })
+const Karla = localFont({ src: './Karla-VariableFont_wght.ttf' })
 
 const InvitationBanner = () => {
     return (
         <section className={`${styles.banner}`}>
             <div>
                 <div className={styles.banner_details}>
-                    <img src="invitations-files/bride-and-groom/pexels-mathias-bruun-nielsen-176519853-25304390.jpg" alt="Example Image" />
-                    <h1 className={`${styles.text_overlay} ${mon.className}`}>Jane & Jhon</h1>
-                    <p className={`${styles.text_container}`}>
-                        Lorem ipsum dolor sit amet consectetur adipiscing elit phasellus vel..
+                    <Image 
+                        className={`${styles.image} ${styles.large_image}`} 
+                        width={280} height={420} src="/invitations-files/bride-and-groom/pexels-yulia-polyakova-73722901-10095638.jpg" 
+                        alt="Couple portrait" />
+                    <Image 
+                        className={`${styles.image} ${styles.small_image}`} 
+                        width={240} height={360} src="/invitations-files/bride-and-groom/pexels-yulia-polyakova-73722901-10095638.jpg" 
+                        alt="Couple portrait" />
+                    <span className={styles.image_overlay} />
+                    <h1 className={`${styles.text_overlay} ${Montserrat.className}`}>Jane & Jhon</h1>
+                    <p className={`${styles.text_container} ${Karla.className}`}>
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit phasellus vel.
                     </p>
                 </div>
 
