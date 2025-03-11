7650ba8480ccb184c20d63ae31ad7a06b5f5624d
alejandro3469 <alejandro.perez3469@gmail.com>
2025-03-10 23:13:22 -0600

    Add invitation sections and main content

--- app/components/wedding-invitations/Cristian&Vanessa/components/Banner.tsx
+++ app/components/wedding-invitations/Cristian&Vanessa/components/Banner.tsx
@@ -60,8 +60,6 @@ const Banner = () => {
                         />
                     </div>
                 </div>
-
-
             </div>
 
 
@@ -85,7 +83,144 @@ const Banner = () => {
                     <p className={styles.secondary}>
                         {
                             "Queremos compartir con ustedes este momento tan especial " +
-                            "y celebrar juntos la unión de nuestras vidas\""
+                            "y celebrar juntos la unión de nuestras vidas"
+                        }
+                    </p>
+                </div>
+            </section>
+
+            <div className={styles.bannerContainer}>
+                <div className={styles.banner}>
+                    <div className={styles.bannerImage}>
+                        <Image
+                            src="/invitations-files/Cristian&Vanessa/IMG_0499.jpeg"
+                            alt="Banner Image"
+                            layout="fill"
+                            objectFit="cover"
+                            priority
+                        />
+                    </div>
+                </div>
+            </div>
+
+
+            <section className={`${styles.initialsSectionContainer}`}>
+                <div className={`${styles.quoteText} ${lora.variable}`}>
+                    <p className={styles.normal}>
+                        {
+                            "Los esperamos el dia:"
+                        }
+                    </p>
+                    <p className={styles.normal}>
+                        {
+                            "03 de Mayo de 2025"
+                        }
+                    </p>
+                    <p className={styles.normal}>
+
+                    </p>
+                </div>
+            </section>
+
+            <div className={styles.bannerContainer}>
+                <div className={styles.banner}>
+                    <div className={styles.bannerImage}>
+                        <Image
+                            src="/invitations-files/Cristian&Vanessa/IMG_0500.jpeg"
+                            alt="Banner Image"
+                            layout="fill"
+                            objectFit="cover"
+                            priority
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <section className={`${styles.initialsSectionContainer}`}>
+                <div className={`${styles.quoteText} ${lora.variable}`}>
+                    <p className={styles.accent}>
+                        {
+                            "Ceremonia religiosa:"
+                        }
+                    </p>
+                    <p className={styles.normal}>
+                        {
+                            " iglesia san Lorenzo "
+                        }
+                    </p>
+                    <a href={"https://maps.app.goo.gl/gn5toBJnMNhyna5G6?g_st=iw"} className={styles.normal}>
+                        {
+                            " Dirección av valle de Juárez , san Lorenzo 32320 "
+                        }
+                    </a>
+                    <p className={styles.normal}>
+                        {
+                            "13:00 pm "
+                        }
+                    </p>
+                </div>
+            </section>
+
+            <div className={styles.bannerContainer}>
+                <div className={styles.banner}>
+                    <div className={styles.bannerImage}>
+                        <Image
+                            src="/invitations-files/Cristian&Vanessa/IMG_0501.jpeg"
+                            alt="Banner Image"
+                            layout="fill"
+                            objectFit="cover"
+                            priority
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <section className={`${styles.initialsSectionContainer}`}>
+                <div className={`${styles.quoteText} ${lora.variable}`}>
+                    <p className={styles.accent}>
+                        {
+                            "Recepcion:"
+                        }
+                    </p>
+                    <a href={"https://maps.app.goo.gl/V2nbhcbq34UYKdr27?g_st=iw"} className={styles.normal}>
+                        {
+                            "Jardín quinta los cerezos "
+                        }
+                    </a>
+                    <p className={styles.normal}>
+                        {
+                            "privada la rosita \n" +
+                            "#1492 arcadas 32543 "
+                        }
+                    </p>
+                    <p className={styles.normal}>
+                        {
+                            "21:00pm a 2:00am"
+                        }
+                    </p>
+                </div>
+            </section>
+
+            <section className={`${styles.initialsSectionContainer}`}>
+                <div className={`${styles.quoteText} ${lora.variable}`}>
+                    <p className={styles.accent}>
+                        {
+                            "Codigo de vestimenta:"
+                        }
+                    </p>
+                    <p className={styles.normal}>
+                        {
+                            "Formal"
+                        }
+                    </p>
+                    <p className={styles.normal}>
+                        {
+                            "Hagamos que todo luzca como siempre lo soñamos"
+                        }
+                    </p>
+                    <p className={styles.normal}>
+                        {
+                            "Por favor evita los siguienes colores"
                         }
                     </p>
                 </div>
@@ -94,20 +229,57 @@ const Banner = () => {
 
             <section className={`${styles.initialsSectionContainer}`}>
                 <div className={`${styles.quoteText} ${lora.variable}`}>
-                    <p className={styles.normal}>
+                    <p className={styles.accent}>
                         {
-                            "Pasamos la vida buscándonos sin encontrarnos.\n " +
-                            "Un día, la casualidad hizo que nos conociéramos\n " +
-                            "y diez años después queremos celebrar nuestro día más especial con vosotros"
+                            "El gran regalo es contar con tu presencia"
                         }
                     </p>
                     <p className={styles.normal}>
                         {
-                            "Los esperamos el dia: Sabado 16 de noviembre"
+                            "Pero si tu deseo es contribuir con nuestra felicidad estaremos muy agradecidos"
                         }
                     </p>
+                    <a href={"https://mesaderegalos.liverpool.com.mx/milistaderegalos/51630326"} className={styles.normal}>
+                        {
+                            "Mesa de regalos"
+                        }
+                    </a>
                     <p className={styles.normal}>
+                        {
+                            "Por favor evita los siguienes colores"
+                        }
+                    </p>
+                </div>
+            </section>
 
+            <section className={`${styles.initialsSectionContainer}`}>
+                <div className={`${styles.quoteText} ${lora.variable}`}>
+                    <p className={styles.accent}>
+                        {
+                            "¿Contamos contigo?"
+                        }
+                    </p>
+                    <p className={styles.normal}>
+                        {
+                            "Porfavor confirma tu asistencia por Whatsapp"
+                        }
+                    </p>
+                    <p className={styles.normal}>
+                        {
+                            "Queremos que nuestra boda sea una noche especial para que todos se relajen y disfruten. " +
+                            "Por ello, hemos decidido que el evento sea solo para adultos. " +
+                            "¡Esperamos celebrar con ustedes!"
+                        }
+                    </p>
+                </div>
+            </section>
+
+            <section className={`${styles.initialsSectionContainer}`}>
+                <div className={`${styles.quoteText} ${lora.variable}`}>
+                    <p className={styles.accent}>
+                        {
+                            "Te esperamos!"
+                        }
                     </p>
                 </div>
             </section>
