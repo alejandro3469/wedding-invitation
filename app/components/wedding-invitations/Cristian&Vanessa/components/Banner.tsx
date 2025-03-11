e8d4df651886a5256fdf0ba97fcfa7d695e391f8
alejandro3469 <alejandro.perez3469@gmail.com>
2025-03-11 09:44:12 -0600

    Update location styles

--- app/components/wedding-invitations/Cristian&Vanessa/components/Banner.tsx
+++ app/components/wedding-invitations/Cristian&Vanessa/components/Banner.tsx
@@ -116,18 +116,18 @@ const Banner = () => {
 
             <section className={`${styles.initialsSectionContainer}`}>
                 <div className={`${styles.quoteText} ${lora.variable}`}>
+                    <svg className={styles.icon} data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"></path>
+                    </svg>
                     <p className={styles.normal}>
                         {
                             "Los esperamos el dia:"
                         }
                     </p>
-                    <p className={styles.normal}>
+                    <p className={styles.accent}>
                         {
-                            "03 de Mayo de 2025"
+                            "Sabado 03 de Mayo de 2025"
                         }
-                    </p>
-                    <p className={styles.normal}>
-
                     </p>
                 </div>
             </section>
@@ -148,19 +148,23 @@ const Banner = () => {
 
             <section className={`${styles.initialsSectionContainer}`}>
                 <div className={`${styles.quoteText} ${lora.variable}`}>
-                    <p className={styles.accent}>
+                    <p className={styles.normal}>
                         {
-                            "Ceremonia religiosa:"
+                            "Ceremonia religiosa"
                         }
                     </p>
-                    <p className={styles.normal}>
+                    <p className={styles.accent}>
                         {
                             " iglesia san Lorenzo "
                         }
                     </p>
-                    <a href={"https://maps.app.goo.gl/gn5toBJnMNhyna5G6?g_st=iw"} className={styles.normal}>
+                    <a href={"https://maps.app.goo.gl/gn5toBJnMNhyna5G6?g_st=iw"} className={`${styles.normal} ${styles.link}`}>
+                        <svg className={styles.map} data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"></path>
+                        </svg>
                         {
-                            " Dirección av valle de Juárez , san Lorenzo 32320 "
+                            "Av. Valle de Juárez, San Lorenzo 32320 "
                         }
                     </a>
                     <p className={styles.normal}>
